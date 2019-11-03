0cbfc51856cdf6197697005b429b2d94c1a0fa8d
Martin Brännstrand <martin.brannstrand@gmail.com>
2019-11-03 11:26:20 +0100

    Detect keyboard input

--- src/index.js
+++ src/index.js
@@ -2,3 +2,10 @@ import App from "./components/App.js"
 
 const app = new App()
 document.getElementById("root").appendChild(app.elements.root)
+
+function detectKeyboardInput() {
+  document.documentElement.classList.add("keyboarduser")
+}
+
+document.addEventListener("keydown", detectKeyboardInput)
+document.addEventListener("keyup", detectKeyboardInput)
